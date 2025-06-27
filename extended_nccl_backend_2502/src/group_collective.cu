f6a756314b81a3f2b14f9f27e64dc54020ee04c0
Strivin0311 <hyp@smail.nju.edu.cn>
2025-06-27 06:54:30 +0000

    implemented group-reduce post-process kernel for version3

--- extended_nccl_backend_2502/src/group_collective.cu
+++ extended_nccl_backend_2502/src/group_collective.cu
@@ -3,6 +3,7 @@
 #endif
 
 #include "../include/group_collective.cuh"
+#include <ATen/cuda/Atomic.cuh>
 
 
 ncclDataType_t to_nccl_data_type(c10::ScalarType type) {
@@ -49,14 +50,20 @@ ncclComm_t to_nccl_comm(torch::cuda::nccl::ncclComm_t var) {
 }
 
 
-#define GROUP_REDUCE_POST_PROCESS_NUM_SMS 32 /* following nccl comm kernel, which consumes 32 SMs at most */
-#define GROUP_REDUCE_POST_PROCESS_BLOCK_SIZE 1024 /* use the maximum block size */
+#define GROUP_REDUCE_POST_PROCESS_TEST /* if set, no limit to grid size to test the highest HBM throughput */
 
-// #define GROUP_REDUCE_POST_PROCESS_TEST /* if set, no limit to grid size to test the highest HBM throughput */
+#define GROUP_REDUCE_POST_PROCESS_NUM_SMS 32 /* following nccl comm kernel, which consumes 32 SMs at most */
+
+#ifdef GROUP_REDUCE_POST_PROCESS_TEST
+#define GROUP_REDUCE_POST_PROCESS_BLOCK_SIZE 512
+#else
+#define GROUP_REDUCE_POST_PROCESS_BLOCK_SIZE 1024 /* use the maximum block size */
+#endif
 
 // #define GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION 0 // 0: per element per thread
 // #define GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION 1 // 1: per row per thread
 #define GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION 2 // 2: per row per block
+// #define GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION 3 // 3: per row per block but atomic add
 
 
 namespace torch::cuda::nccl {
@@ -187,7 +194,7 @@ namespace torch::cuda::nccl {
                 // load the original data of current col to be reduced to
                 scalar_t recv_reduce_data = *recv_data_ptr_this_col;
 
-                // get the corr ptr of first partial data
+                // get the corr. ptr of first partial data
                 auto repeated_recv_data_ptr_this_col = (repeated_recv_data_ptr + col_idx);
 
                 // load and reduce each corr. partial data
@@ -222,7 +229,7 @@ namespace torch::cuda::nccl {
 
         size_t split_idx = 0;
         for (auto row_idx = bid; row_idx < seqlen; row_idx += num_blocks_per_grid) {
-            if (tid_in_block == 0) {
+            if (!tid_in_block) {
                 // only the thread 0 in this block searchs for split idx that the current idx belongs
                 split_idx = binary_search_split(
                     d_cu_split_size_list,
@@ -247,22 +254,6 @@ namespace torch::cuda::nccl {
             auto num_repeats = shared_split_info[4];
             auto recv_split_offset_to_idx = row_start - recv_split_start;
 
-            // // search for split idx that the current idx belongs
-            // split_idx = binary_search_split(
-            //     d_cu_split_size_list,
-            //     split_idx,
-            //     num_splits,
-            //     row_idx,
-            //     1
-            // );
-
-            // auto row_start = row_idx * stride0;
-            // auto recv_split_start = d_cu_split_size_list[split_idx] * stride0;
-            // auto repeated_recv_split_start = d_repeated_cu_split_size_list[split_idx] * stride0;
-            // auto recv_split_size = d_split_size_list[split_idx] * stride0;
-            // auto num_repeats = d_num_repeats_list[split_idx];
-            // auto recv_split_offset_to_idx = row_start - recv_split_start;
-
             // get the row start ptr of recv_buffer
             scalar_t* recv_data_ptr = (recv_buffer + row_start);
 
@@ -277,7 +268,7 @@ namespace torch::cuda::nccl {
                 // load the original data of current col to be reduced to
                 scalar_t recv_reduce_data = *recv_data_ptr_this_col;
 
-                // get the corr ptr of first partial data
+                // get the corr. ptr of first partial data
                 auto repeated_recv_data_ptr_this_col = (repeated_recv_data_ptr + col_idx);
 
                 // load and reduce each corr. partial data
@@ -288,6 +279,71 @@ namespace torch::cuda::nccl {
              
                 // write the reduced data back to recv_buffer
                 *recv_data_ptr_this_col = recv_reduce_data;
+            }
+        }
+    }
+    #elif GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION == 3
+    /** NOTE: this version uses each block to process a single row, but using atomic add */
+    template <typename scalar_t>
+    __global__ void group_reduce_nccl_post_process_kernel( // repeat-interleaved range reduce
+        scalar_t* recv_buffer,
+        const scalar_t* repeated_recv_buffer,
+        const int64_t* d_split_size_list,
+        const int64_t* d_num_repeats_list,
+        const int64_t* d_cu_split_size_list,
+        const int64_t* d_repeated_cu_split_size_list,
+        size_t seqlen,
+        size_t num_splits,
+        size_t stride0
+    ) {
+        extern __shared__ size_t shared_split_info[];
+
+        size_t bid = blockIdx.x, tid_in_block = threadIdx.x;
+        size_t num_blocks_per_grid = gridDim.x, num_threads_per_block = blockDim.x;
+
+        size_t split_idx = 0;
+        for (auto row_idx = bid; row_idx < seqlen; row_idx += num_blocks_per_grid) {
+            if (!tid_in_block) {
+                // only the thread 0 in this block searchs for split idx that the current idx belongs
+                split_idx = binary_search_split(
+                    d_cu_split_size_list,
+                    split_idx,
+                    num_splits,
+                    row_idx,
+                    1
+                );
+                // thread 0 gets the info about this split and writes it to shared memory
+                shared_split_info[0] = row_idx * stride0; // row_start
+                shared_split_info[1] = d_cu_split_size_list[split_idx] * stride0; // recv_split_start
+                shared_split_info[2] = d_repeated_cu_split_size_list[split_idx] * stride0; // repeated_recv_split_start
+                shared_split_info[3] = d_split_size_list[split_idx] * stride0; // recv_split_size
+                shared_split_info[4] = d_num_repeats_list[split_idx]; // num_repeats
+            } __syncthreads(); // all threads in this block wait for the same split info to be ready
+
+            // get the info about this split
+            auto row_start = shared_split_info[0];
+            auto recv_split_start = shared_split_info[1];
+            auto repeated_recv_split_start = shared_split_info[2];
+            auto recv_split_size = shared_split_info[3];
+            auto num_repeats = shared_split_info[4];
+            auto recv_split_offset_to_idx = row_start - recv_split_start;
+
+            // get the row start ptr of recv_buffer
+            scalar_t* recv_data_ptr = (recv_buffer + row_start);
+
+            // get the row start ptr of first partial split of repeated_recv_buffer
+            const scalar_t* repeated_recv_data_ptr = (repeated_recv_buffer + repeated_recv_split_start + recv_split_offset_to_idx);
+
+            // for-loop this row for num_repeats times
+            for (auto rep_col_idx = tid_in_block; rep_col_idx < stride0 * num_repeats; rep_col_idx += num_threads_per_block) {
+                // get the col idx and repeat idx
+                size_t rep_idx = rep_col_idx / stride0;
+                size_t col_idx = rep_col_idx % stride0;
+
+                gpuAtomicAdd(
+                    recv_data_ptr + col_idx,
+                    *(repeated_recv_data_ptr + col_idx + rep_idx * recv_split_size)
+                );
             }
         }
     }
@@ -467,7 +523,7 @@ namespace torch::cuda::nccl {
         #endif
         dim3 blockDims(GROUP_REDUCE_POST_PROCESS_BLOCK_SIZE);
 
-        #if GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION == 2
+        #if GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION >= 2
         size_t sharedMemSize = 5 * sizeof(size_t);
         #else
         size_t sharedMemSize = 0;
@@ -482,6 +538,8 @@ namespace torch::cuda::nccl {
             "group_reduce_nccl_post_process_kernel_v1",
             #elif GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION == 2
             "group_reduce_nccl_post_process_kernel_v2",
+            #elif GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION == 3
+            "group_reduce_nccl_post_process_kernel_v3",
             #else
             #error "Unsupported GROUP_REDUCE_POST_PROCESS_KERNEL_VERSION"
             #endif
